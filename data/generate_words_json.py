b87d32ae47b964ee9c24dff25cf5a9cfa7dbe96e
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2019-04-03 14:55:27 +0100

    [IMP] Words JSON file Python generator

---
+++ data/generate_words_json.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import json
+
+words_list = []
+
+with open('lista.txt', 'r') as in_file:
+    for cnt, line in enumerate(in_file):
+        data = {}
+        en, pl = line.split('-')
+        data['en'] = str(en).strip()
+        data['pl'] = str(pl).strip()
+        words_list += [data]
+
+with open('words.json', 'w') as out_file:
+    out_file.write(json.dumps(words_list).replace("},", '},\n'))
+
+print('JSON generator finished.')
