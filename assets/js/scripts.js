4cd566ac2dbc7c94203b0490ed8168f824495c77
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2019-04-03 11:15:55 +0100

    [IMP] "Next" functionality
    Added Next button for another word to translate
    Refactored code to use some functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,30 +13,52 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
+function fetch_english() {
+    if (words) {
+        let number_en = Math.floor(Math.random() * words.length);
+        document.getElementById("english_word").innerHTML = words[number_en]['word'];
+        document.getElementById("polish_answer").innerHTML = words[number_en]['pol'];
+    } else {
+        alert("Error: Words not loaded!");
+    }
+}
+
+function fetch_polish() {
+    if (words) {
+        let number_en = Math.floor(Math.random() * words.length);
+        document.getElementById("polish_word").innerHTML = words[number_en]['word'];
+        document.getElementById("english_answer").innerHTML = words[number_en]['pol'];
+    } else {
+        alert("Error: Words not loaded!");
+    }
+}
+
 function init() {
     loadJSON(function (response) {
         // Parse JSON string into object
         words = JSON.parse(response);
-
-        if (words) {
-
-            let number_en = Math.floor(Math.random() * words.length);
-            document.getElementById("english_word").innerHTML = words[number_en]['word'];
-            document.getElementById("polish_answer").innerHTML = words[number_en]['pol'];
-
-            let number_pl = Math.floor(Math.random() * words.length);
-            document.getElementById("polish_word").innerHTML = words[number_pl]['pol'];
-            document.getElementById("english_answer").innerHTML = words[number_pl]['word'];
-        }
+        fetch_english();
+        fetch_polish();
     });
 }
 
-init();
+var polish_answer = document.getElementById("polish_answer"),
+    english_answer = document.getElementById("english_answer");
 
 document.getElementById("reveal_polish").addEventListener("click", function () {
-    document.getElementById("polish_answer").classList.remove("hidden");
+    polish_answer.classList.remove("hidden");
+});
+document.getElementById("next_english").addEventListener("click", function () {
+    polish_answer.classList.add("hidden");
+    fetch_english();
 });
 
 document.getElementById("reveal_english").addEventListener("click", function () {
-    document.getElementById("english_answer").classList.remove("hidden");
+    english_answer.classList.remove("hidden");
 });
+document.getElementById("next_polish").addEventListener("click", function () {
+    english_answer.classList.add("hidden");
+    fetch_polish();
+});
+
+init();
